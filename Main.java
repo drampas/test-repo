ebf7bcd26ab93df5187f1efa07ab48b2a431351e
drampas <drampasaggelos@gmail.com>
2023-01-25 14:37:40 +0200

    additions to main

--- Main.java
+++ Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("hello git");
+
+        for(int i=0;i<4;i++){
+            System.out.println(i+1+" and");
+        }
     }
 }
